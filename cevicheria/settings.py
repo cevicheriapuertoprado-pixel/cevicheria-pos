4cd10aee918073ee1bf56ffcc32e2c9b3e9a8915
PUERTOPRADO <cevicheriapuertoprado@gmail.com>
2025-09-13 01:27:01 -0500

    Preparando para render

--- cevicheria/settings.py
+++ cevicheria/settings.py
@@ -81,20 +81,14 @@ WSGI_APPLICATION = 'cevicheria.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-import dj_database_url
 import os
+import dj_database_url
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get(
-            "DATABASE_URL",
-            "postgresql://cevicheria_db_user:3ax98PgcPBnBWtzzZZJ3YUzPXUtLIIi3@dpg-d32g5pvdiees738o82hg-a.oregon-postgres.render.com/cevicheria_db"
-        ),
-        conn_max_age=600,
-        ssl_require=True
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
     )
 }
-
 
 
 
