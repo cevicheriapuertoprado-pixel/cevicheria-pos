2fac49f2fa7bce41108e7fde1f32ef875b2e70d8
PUERTOPRADO <cevicheriapuertoprado@gmail.com>
2025-09-13 01:01:12 -0500

    Configurar PostgreSQL en Render

--- cevicheria/settings.py
+++ cevicheria/settings.py
@@ -86,7 +86,7 @@ import os
 
 DATABASES = {
     "default": dj_database_url.config(
-        default="postgresql://cevicheria_db_user:3ax98PgcPBnBWtzzZZJ3YUzPXUtLIIi3@dpg-d32g5pvdiees738o82hg-a.oregon-postgres.render.com/cevicheria_db",
+        default=os.environ.get("DATABASE_URL", "postgresql://cevicheria_db_user:3ax98PgcPBnBWtzzZZJ3YUzPXUtLIIi3@dpg-d32g5pvdiees738o82hg-a.oregon-postgres.render.com/cevicheria_db"),
         conn_max_age=600,
         ssl_require=True
     )
